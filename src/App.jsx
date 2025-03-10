70e5e940beffb2c23d4d71d0a9d8ccecc2455177
mabel <mabelare293@gmail.com>
2025-03-10 01:07:30 +0100

    bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage.jsx";
 import Test from "./Test.jsx";
 import Pusher from "./Pusher.jsx";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/test" element={<Test />} />
 
       <Route path="/pusher" element={<Pusher />} />
---
+++ src/LandingPage.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+const LandingPage = () => {
+  return (
+    <div className="relative flex items-center justify-center h-screen">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute object-cover w-full h-full"
+      >
+        <source src="/document_5940284188090439704.mp4" type="video/mp4" />
+      </video>
+      <div className=" flex flex-col items-center justify-center relative z-10 ">
+        <h1 className="text-3xl font-bold text-white mb-8 lg:mt-48  ">
+          Welcome to our platform
+        </h1>
+        <div className="space-x-6">
+          <Link to="/Test">
+            <button
+              className="px-6 py-3 bg-blue-500 text-white rounded-lg
+          shadow-md hover:bg-pink-600  transition"
+            >
+              For individuals
+            </button>
+          </Link>
+          <Link to="/Pusher">
+            <button
+              className="px-6 py-3 bg-green-500 text-white rounded-lg
+          shadow-md hover:bg-pink-600  transition"
+            >
+              For Groups
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
